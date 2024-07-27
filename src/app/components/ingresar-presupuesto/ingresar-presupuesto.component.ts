9750c3b395236debc0a599d0f9ea877e4cc79d4c
Vale L <106398144+vlysi@users.noreply.github.com>
2024-07-26 23:01:42 -0300

    first commit

---
+++ src/app/components/ingresar-presupuesto/ingresar-presupuesto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingresar-presupuesto',
+  templateUrl: './ingresar-presupuesto.component.html',
+  styleUrls: ['./ingresar-presupuesto.component.css']
+})
+export class IngresarPresupuestoComponent {
+
+}
